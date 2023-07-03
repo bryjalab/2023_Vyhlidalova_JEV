14203e5850125564455cbc67a8f1b6f559083401
Hana Plešingerová <madro@ofizcave01.sci.muni.cz>
2023-07-03 14:11:55 +0200

    survival analysis updated

--- code/14_survival-analysis.R
+++ code/14_survival-analysis.R
@@ -18,99 +18,21 @@ os <- read.csv2(here("data", "survival-data.csv")) %>%
 # Load data with MS intensities 
 load(here("analysis", "05_filter-out-B-samples", "05_data.filtered.Rdata")) 
 
-# Load 157 proteins specific for cancer patients
-load(here("output", "09_controls-addition", "09_patient-specific-data.Rdata"))
+# Load 45 cell-specific proteins
+load(here("outputs", "11_RNAseq-comparison_II", "11_SEC-UC-long-df.RData"))
+fac <- SEC_long$rowname %>% 
+  unique() %>% 
+  c("MRC1", "IDH2", "FAS", "ITGB8")
 
-# Filter MS intensities for 157 patient-specific proteins
-data.filtered.157 <- data.filtered %>% 
-  filter(Suggested.Symbol %in% patient.specific$Suggested.Symbol)
-data.filtered.157[is.na(data.filtered.157)] <- 0
+
+# Filter MS intensities for 45 cell-specific proteins
+data.filtered.45 <- data.filtered %>% 
+  filter(Suggested.Symbol %in% fac)
+data.filtered.45[is.na(data.filtered.45)] <- 0
 
 # Create a new directory for processed data:
 dir.create(here("output", "14_survival-analysis"))
 
-############################# SEC SAMPLES ######################################
-
-# Select SEC samples
-ms.sec <- data.filtered.157  %>% 
-  dplyr::select(c(starts_with("S"), "ID", -"Suggested.Symbol")) %>% 
-  column_to_rownames(var = "ID") %>%  
-  t() %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var = "Patient") %>% 
-  mutate(Patient = as.factor(str_remove(string = Patient, pattern = "S"))) %>% 
-  full_join(x = os, y = ., by = "Patient")
-
-# Extract protein names
-fac <- ms.sec %>% 
-  dplyr::select(-c(Patient:Dead)) %>% 
-  names()
-
-# Create survival object
-ms.sec$OS <- Surv(ms.sec$OS.days, ms.sec$Dead) 
-
-# Create table for individual cut-offs
-tab <- data.frame(fac = NA, cut = NA, p.value = NA, HR = NA, LCL = NA, UCL = NA)
-
-# Create table for best cut-offs in individual proteins
-tab_sel_sec <- data.frame(fac = NA, cut = NA, p.value = NA, HR = NA, LCL = NA, UCL = NA)
-
-# Compute survival analysis for each cut-off in each individual protein
-for (j in c(1:(length(fac)))) {
-  
-  # Extract data for an individual protein
-  f_str <- fac[j]
-  f <-  eval(parse(text = paste0("ms.sec$",f_str))) 
-  f <- as.numeric(as.character(factor(f)))  
-  
-  if (length(f) > 0)
-  {
-    p = NA
-    cut = NA
-    HR = NA
-    LCL = NA
-    UCL = NA
-
-    # Sort unique MS intensities
-    f_sort <- f %>% as.data.frame() %>% distinct()
-    f.sort = sort(f_sort$.)
-    
-    # Compute survival analysis for individual cut-offs
-    i = 1
-    for (i in c(1:(length(f.sort) - 1)))
-    {
-      groups.f = rep(NA, length(f))
-      c <- (f.sort[i] + f.sort[i + 1]) / 2
-      groups.f[which(f < c)] = paste0("<", as.character(c))
-      groups.f[which(f >= c)] = paste0(">", as.character(c))
-      
-      fit.f = survfit(OS ~ groups.f, data = ms.sec)
-      test.f = survdiff(OS ~ groups.f, rho = 0, data=ms.sec)
-      cox.f = coxph(OS ~ groups.f, data=ms.sec)
-      
-      p[i] = 1 - pchisq(test.f[[5]], df = 1)
-      
-      cut[i] = (f.sort[i] + f.sort[i + 1]) / 2
-
-      HR[i] = summary(cox.f)$conf.int[1]
-      LCL[i] = summary(cox.f)$conf.int[3]
-      UCL[i] = summary(cox.f)$conf.int[4]
-    }
-    
-    # Save results for each protein
-    tab1 <- data.frame(fac = f_str, cut = cut, p.value = p, HR = HR, LCL = LCL, UCL = UCL) 
-    tab <<- rbind(tab, tab1, c(NA))
-    
-    #  Extract best cut-off for each protein
-    tab_min <- tab1[which(min(tab1$p.value) == tab1$p.value),] 
-    tab_sel_sec <<- rbind(tab_sel_sec, tab_min) 
-  }
-}
-
-# Omit first row with NA values and compute adjusted P-value
-tab_sel_sec <- tab_sel_sec[-1,] 
-tab_sel_sec$adj.p.val <- p.adjust(tab_sel_sec$p.value, method = "fdr", n = length(tab_sel_sec$p.value)) 
-  
 
 ############################# UC SAMPLES ######################################
 
@@ -128,10 +50,10 @@ ms.uc <- data.filtered.157 %>%
 ms.uc$OS <- Surv(ms.uc$OS.days, ms.uc$Dead) 
 
 # Create table for individual cut-offs
-tab <- data.frame(fac = NA, cut = NA, p.value = NA, HR = NA, LCL = NA, UCL = NA)
+tab <- data.frame(fac = NA, cut = NA, p.value = NA, HR = NA, LCL = NA, UCL = NA, bellow.cut = NA, above.cut = NA)
 
 # Create table for best cut-offs in individual proteins
-tab_sel_uc <- data.frame(fac = NA, cut = NA, p.value = NA, HR = NA, LCL = NA, UCL = NA)
+tab_sel_uc <- data.frame(fac = NA, cut = NA, p.value = NA, HR = NA, LCL = NA, UCL = NA, bellow.cut = NA, above.cut = NA)
 
 #  Compute survival analysis for each cut-off in each individual protein
 for (j in c(1:(length(fac)))) {
@@ -148,6 +70,8 @@ for (j in c(1:(length(fac)))) {
     HR = NA
     LCL = NA
     UCL = NA
+    bellow.cut = NA
+    above.cut = NA
     
     # Sort unique MS intensities
     f_sort <- f %>% as.data.frame() %>% distinct()
@@ -174,33 +98,28 @@ for (j in c(1:(length(fac)))) {
       LCL[i] = summary(cox.f)$conf.int[3]
       UCL[i] = summary(cox.f)$conf.int[4]
       
-      
+       bellow.cut[i] = fit.f[[1]][1]
+       above.cut[i] = fit.f[[1]][2]
     }
     
     # Save results for each protein
-    tab1 <- data.frame(fac = f_str, cut = cut, p.value = p, HR = HR, LCL = LCL, UCL = UCL) 
+    tab1 <- data.frame(fac = f_str, cut = cut, p.value = p, HR = HR, LCL = LCL, UCL = UCL, bellow.cut = bellow.cut, above.cut = above.cut) 
     tab <<- rbind(tab, tab1, c(NA))
     
+    # Filter cut-off dividing into groups with minimal size  = 3 patients and set minimal cut-off at 100 000
+    tab2 <- tab1 %>% 
+      filter(bellow.cut > 2 & bellow.cut < 9) %>% 
+      filter(cut >= 100000)
+  
     #  Extract best cut-off for each protein
-    tab_min <- tab1[which(min(tab1$p.value) == tab1$p.value),] 
+    tab_min <- tab2[which(min(tab2$p.value) == tab2$p.value),] 
     tab_sel_uc <<- rbind(tab_sel_uc, tab_min) 
   }
 }
 
-# Omit first row with NA values and compute adjusted P-value
+# Omit first row with NA values 
 tab_sel_uc <- tab_sel_uc[-1,] 
-tab_sel_uc$adj.p.val <- p.adjust(tab_sel_uc$p.value, method = "fdr", n = length(tab_sel_uc$p.value)) 
 
-######################### COMBINE RESULTS ######################################
-
-# Join results from SEC and UC samples
-sec.uc <- inner_join(x = tab_sel_sec, y = tab_sel_uc, by = "fac", suffix = c("_sec", "_uc")) %>% 
-  rename(ID = fac) 
-
-# Add gene names
-sec.uc <- data.filtered %>% 
-  select(c(Suggested.Symbol, ID)) %>% 
-  inner_join(x = ., y = sec.uc, by = "ID")
 
 #  Export results
-write.csv(sec.uc, file = here("output", "14_survival-analysis", "14_157-proteins-best-cut-offs.csv"))
+write.csv(sec.uc, file = here("output", "14_survival-analysis", "14_45-proteins-best-cut-offs.csv"))
